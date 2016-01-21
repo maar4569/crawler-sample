1f42788a383aa8255aaef1bb5b2cb03842868433
maar4569 <maar4569@gmail.com>
2016-01-21 04:37:35 +0000

    add mylogging module

---
+++ log.py
@@ -0,0 +1,42 @@
+import inspect
+import sys
+from  logging  import NOTSET,INFO,DEBUG,WARNING,ERROR,getLogger,Formatter,StreamHandler,FileHandler
+
+class myLogger(object):
+    def __init__(self,name):
+         self._log = getLogger(name)
+         self._classname = name
+    
+    def debug(self,message):
+        try:
+            methodname = sys._getframe(1).f_code.co_name
+            self._log.debug(self._classname + "." + methodname + "() : " +  message)
+        except Exception as e:
+            self._log.exception("error occured.")
+            raise Exception(e.args)
+
+    def warning(self,message):
+        try:
+            methodname = sys._getframe(1).f_code.co_name
+            self._log.warning(self._classname + "." + methodname + "() : " +  message)
+        except Exception as e:
+            self._log.exception("error occured.")
+            raise Exception(e.args)
+
+    def info(self,message):
+        try:
+            methodname = sys._getframe(1).f_code.co_name
+            self._log.info(self._classname + "." + methodname + "() : " +  message)
+        except Exception as e:
+            self._log.exception("error occured.")
+            raise Exception(e.args)
+
+    def error(self,message):
+        try:
+            methodname = sys._getframe(1).f_code.co_name
+            self._log.error(self._classname + "." + methodname + "() : " +  message)
+        except Exception as e:
+            self._log.exception("error occured.")
+            raise Exception(e.args)
+
+
