7ab26c6ee728d466598125278c521c88b5b4c8b0
maar4569 <maar4569@gmail.com>
2016-02-19 08:58:54 +0000

    make apps dir and move files.

--- run_test.sh
+++ run_test.sh
@@ -2,18 +2,21 @@ export PYTHONPATH=$PYTHONPATH:`pwd -P`
 #export PYTHONPATH=$PYTHONPATH:../
 
 
-#OK
-#python tests/test_relatedsite.py
-#OK
-#python tests/test_outputwriter.py
-#OK
-#python tests/test_scoreler.py
-#OK
-#python tests/test_scrapyer.py
+#after changed directory.
+#OK python tests/test_callprocess.py
 
-#OK
-#python tests/test_callprocess.py
+#OK python tests/test_relatedsite.py
 
+#OK python tests/test_scoreler.py
+
+#OK python tests/test_outputwriter.py
+
+#OK python tests/test_scrapyer.py
+
+#OK python tests/test_config.py
+
+#--------------------------
+#before
 
 #processing
 #python tests/test_categorymatcher.py
@@ -22,10 +25,11 @@ export PYTHONPATH=$PYTHONPATH:`pwd -P`
 #python tests/test_outputformatter.py
 
 #processing
-python tests/test_crawler.py
+#python tests/test_crawler.py
 
 
-#JSONStreamwriter test 
-#python test.py
+#logger processing
+#python tests/test_pythonlogger.py
+#python tests/test_log.py
 
 
